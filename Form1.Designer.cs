37750c27a92e15ab20f20a62581add464935a107
dalbay <fadalbay@gmail.com>
2019-09-21 21:09:51 -0400

    upload app files

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ExceptionHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.errorProviderEmpty = new System.Windows.Forms.ErrorProvider(this.components);
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblFinalResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderEmpty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Traditional Averaging Program";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Add a Score:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(128, 68);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 24);
+            this.txtScore.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(128, 98);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 33);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // errorProviderEmpty
+            // 
+            this.errorProviderEmpty.ContainerControl = this;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(125, 149);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(50, 18);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Result";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Final Grade";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblFinalResult
+            // 
+            this.lblFinalResult.AutoSize = true;
+            this.lblFinalResult.Location = new System.Drawing.Point(463, 105);
+            this.lblFinalResult.Name = "lblFinalResult";
+            this.lblFinalResult.Size = new System.Drawing.Size(85, 18);
+            this.lblFinalResult.TabIndex = 7;
+            this.lblFinalResult.Text = "Final Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 259);
+            this.Controls.Add(this.lblFinalResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderEmpty)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ErrorProvider errorProviderEmpty;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblFinalResult;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace ExceptionHandling
+{
+    public partial class Form1 : Form
+    {
+        static ArrayList scoreCard;
+        public Form1()
+        {
+            InitializeComponent();
+            // initialize the scoreCard ArrayList
+            scoreCard = new ArrayList();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblResult.Text = "";
+                var score = txtScore.Text;
+                if (score == string.Empty)   // checking input not empty
+                {
+                    errorProviderEmpty.SetError(txtScore, "Please provide a Score");
+                    lblResult.Text = "No score added!";
+                    throw new NullReferenceException("Null Reference Exception! \nInput Score Value Expected.");
+                }
+                else if (Convert.ToInt32(score) > 100) // checking max input value
+                {
+                    errorProviderEmpty.SetError(txtScore, "Maximum Score is 100");
+                    lblResult.Text = "Add a value between 0 - 100 \nNo score added!";
+                }
+                else
+                {
+                    errorProviderEmpty.SetError(txtScore, "");
+                    scoreCard.Add((Convert.ToUInt32(txtScore.Text))); // checking positive number
+                    lblResult.Text = txtScore.Text + " was succesfully added to your scorecard.";
+                }
+
+            }
+            catch (NotImplementedException e_notImplemented)
+            {
+                lblResult.Text = e_notImplemented.ToString();
+                errorProviderEmpty.SetError(txtScore, "Internal Error!");
+            }
+            catch (FormatException e_format)
+            {
+                lblResult.Text = e_format.Message.ToString() + "\nAdd numeric score!";
+                errorProviderEmpty.SetError(txtScore, "Enter a numeric value");
+            }
+            catch (OverflowException e_overflow)
+            {
+                lblResult.Text = e_overflow.Message.ToString() + "\nAdd a value between 0 - 100 \nNo score added!";
+                errorProviderEmpty.SetError(txtScore, "Minimum Score value is 0");
+            }
+            catch (NullReferenceException e_null)
+            {
+                lblResult.Text = e_null.Message.ToString();
+            }
+        }
+        public static string CalculateRubic()
+        {
+            int score = 0;
+            foreach (var item in scoreCard)
+            {
+                score += Convert.ToInt32(item);
+            }
+            var value = Convert.ToInt32(score / scoreCard.Count);
+            var result = "";
+            switch (value)
+            {
+                case int a when a >= 90:
+                    result = "A";
+                    break;
+                case int b when b >= 80:
+                    result = "B";
+                    break;
+                case int c when c >= 70:
+                    result = "C";
+                    break;
+                case int d when d >= 60:
+                    result = "D";
+                    break;
+                default: result = "F"; break;
+            }
+            return "Final Grade: " + value + " - " + result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblFinalResult.Text = CalculateRubic();
+        }
+    }
+}
